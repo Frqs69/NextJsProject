4b4698e65184c934ed260be589ca5e31d82a6882
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-07-12 21:22:41 +0200

    added fetching data from file

--- next-js-project2/pages/index.js
+++ next-js-project2/pages/index.js
@@ -1,8 +1,16 @@
+import path from "path";
+import fs from "fs/promises";
+
 export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
 	return {
 		props: {
-			products: [{ id: "p1", title: "Product 1" }],
+			products: data.products,
 		},
+		revalidate: 10,
 	};
 }
 
