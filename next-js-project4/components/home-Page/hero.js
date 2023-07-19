066e27ee678d3664ad84b1a40d25a81a8e98d270
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-07-19 20:25:17 +0200

    created and styled hero section and navigation

--- next-js-project4/components/home-Page/hero.js
+++ next-js-project4/components/home-Page/hero.js
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 export default function Hero() {
-	return <></>;
+	return (
+		<section className={classes.hero}>
+			<div className={classes.image}>
+				<Image
+					src='/moje.png'
+					alt='An image showing Kamil'
+					width={300}
+					height={300}
+				/>
+			</div>
+			<h1>Hi, I'm Kamil</h1>
+			<p>
+				A blog about web development - especially frontend framework like React
+			</p>
+		</section>
+	);
 }
