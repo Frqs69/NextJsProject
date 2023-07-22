084b0159ddc9e1d4c0a0b7595f04f67a7fd0b3ad
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-07-22 19:17:18 +0200

    Update next.config.js

--- next-js-project4/next.config.js
+++ next-js-project4/next.config.js
@@ -3,12 +3,6 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		mongoDb_user: "kbazyluk123",
-		mongoDb_password: "74pKHpWFWtcJUlEm",
-		mongoDb_clusterName: "cluster0",
-		mongoDb_database: "my-site",
-	},
 };
 
 module.exports = (phase) => {
